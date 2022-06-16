5a3cab0ef40184b6659c4e635c9582ff67676baf
Anders Perrone <perr0372@ahl04.agate.msi.umn.edu>
2022-06-16 13:44:12 -0500

    Update abcd-dicom2bids routines

--- slurm_abcd-dicom2bids_scripts/make_run_files.sh
+++ slurm_abcd-dicom2bids_scripts/make_run_files.sh
@@ -1,6 +1,6 @@
  #!/bin/bash 
-
 set +x 
+
 # determine data directory, run folders, and run templates
 run_folder=`pwd`
 
@@ -8,7 +8,6 @@ dicom2bids_folder="${run_folder}/run_files.dicom2bids"
 dicom2bids_template="template.dicom2bids"
 dicom2bids_subjects="${run_folder}/sub_files.dicom2bids"
 
-email=`echo $USER@umn.edu`
 group=`groups|cut -d" " -f1`
 
 # if processing run folders (sMRI, fMRI,) exist delete them and recreate
@@ -19,10 +18,14 @@ else
 	mkdir -p "${dicom2bids_folder}/logs"
 fi
 
+if [ ! -d ${dicom2bids_subjects} ]; then
+	mkdir -p ${dicom2bids_subjects}
+fi
+
 # counter to create run numbers
 k=0
 
-cat year_2_sub_list.txt | while read line; do
+cat /home/rando149/shared/projects/ABCD_missing_SST/subject_list.txt | while read line; do
 	echo ${line} > ${dicom2bids_subjects}/sub-${line}.txt
 	sed -e "s:SUBJECTID:${line}:g" -e "s:SUBJECTDIR:${dicom2bids_subjects}:g" ${run_folder}/template.dicom2bids > ${dicom2bids_folder}/run${k}
 
@@ -31,5 +34,5 @@ done
 
 chmod +x -R ${dicom2bids_folder} 
 
-sed -e "s:GROUP:${group}:g" -e "s:EMAIL:${email}:g" -i ${run_folder}/slurm_dicom2bids.sh 
+sed -e "s:GROUP:${group}:g" -i ${run_folder}/slurm_dicom2bids.sh 
 
--- slurm_abcd-dicom2bids_scripts/slurm_dicom2bids.sh
+++ slurm_abcd-dicom2bids_scripts/slurm_dicom2bids.sh
@@ -2,11 +2,9 @@
 #SBATCH --job-name=abcd2bids
 #SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=10gb
-#SBATCH --tmp=20gb
+#SBATCH --tmp=40gb
 #SBATCH --output=output_logs/dicom2bids_%A_%a.out
 #SBATCH --error=output_logs/dicom2bids_%A_%a.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=hough129@umn.edu
 #SBATCH -A miran045
 #SBATCH -p amdsmall,small
 

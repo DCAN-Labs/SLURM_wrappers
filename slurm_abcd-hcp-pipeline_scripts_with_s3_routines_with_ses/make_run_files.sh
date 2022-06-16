3b6ef04504024d6b635f728c6ecc46e3ff42911b
Anders Perrone <perr0372@ahl04.agate.msi.umn.edu>
2022-06-16 13:45:24 -0500

    Remove sorted directory from s3 path

--- slurm_abcd-hcp-pipeline_scripts_with_s3_routines_with_ses/make_run_files.sh
+++ slurm_abcd-hcp-pipeline_scripts_with_s3_routines_with_ses/make_run_files.sh
@@ -2,14 +2,14 @@
 
 set +x 
 # determine data directory, run folders, and run templates
-data_dir="/tmp/MSC_BIDS" # where to output data
-data_bucket="s3://MSC_BIDS" # bucket that BIDS data will be pulled from and processed outputs will be pushed to
+RANDOM_HASH=`tr -dc A-Za-z0-9 </dev/urandom | head -c 10 ; echo ''`
+data_dir="/tmp/${RANDOM_HASH}" # where to output data
+data_bucket="s3://ABCD_missing_SST" # bucket that BIDS data will be pulled from and processed outputs will be pushed to
 run_folder=`pwd`
 
 abcd_hcp_pipeline_folder="${run_folder}/run_files.abcd-hcp-pipeline_full"
 abcd_hcp_pipeline_template="template.abcd-hcp-pipeline_full_run"
 
-email=`echo $USER@umn.edu`
 group=`groups|cut -d" " -f1`
 
 # if processing run folders (sMRI, fMRI,) exist delete them and recreate
@@ -23,12 +23,12 @@ fi
 # counter to create run numbers
 k=0
 
-for i in `s3cmd ls ${data_bucket}/sorted/ | awk '{print $2}'`; do
+for i in `s3cmd ls ${data_bucket}/ | awk '{print $2}'`; do
 	# does said folder include subject folder?
 	sub_text=`echo ${i} | awk -F"/" '{print $(NF-1)}' | awk -F"-" '{print $1}'`
 	if [ "sub" = "${sub_text}" ]; then # if parsed text matches to "sub", continue
 		subj_id=`echo ${i} | awk -F"/" '{print $(NF-1)}' | awk  -F"-" '{print $2}'`
-		for j in `s3cmd ls ${data_bucket}/sorted/${sub_text}-${subj_id}/ | awk '{print $2}'`; do
+		for j in `s3cmd ls ${data_bucket}/${sub_text}-${subj_id}/ | awk '{print $2}'`; do
 			ses_text=`echo ${j} |  awk -F"/" '{print $(NF-1)}' | awk -F"-" '{print $1}'` # CHANGE THIS?
 			if [ "ses" = "${ses_text}" ]; then
 				ses_id=`echo ${j} | awk -F"/" '{print $(NF-1)}' | awk  -F"-" '{print $2}'` # CHANGE THIS?

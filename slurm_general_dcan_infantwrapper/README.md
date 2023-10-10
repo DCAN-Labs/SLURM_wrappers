d4823afb2262c9ca203bab873d3346f599efa119
rae McCollum <mccol199@ln1001.mangi.msi.umn.edu>
2023-10-10 11:42:01 -0500

    added infant wrappers

---
+++ slurm_general_dcan_infantwrapper/README.md
@@ -0,0 +1,2 @@
+# slurm_abcd-hcp-pipeline_scripts
+This script pulls down a subject at a time from the s3 then runs DCAN-infant pipeline before pushing it back to the s3
---
+++ slurm_general_dcan_infantwrapper/resources_abcd-hcp-pipeline_infant.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -J dcan-infant-pipeline
+#SBATCH --mem=60gb
+#SBATCH --tmp=100gb
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hough129@umn.edu
+#SBATCH -o output_logs/abcd-hcp-pipeline_infant_%A_%a.out
+#SBATCH -e output_logs/abcd-hcp-pipeline_infant_%A_%a.err
+#SBATCH -A elisonj
+#SBATCH --time=72:00:00
+#SBATCH --ntasks=24
+#SBATCH -p ram256g
+
+cd run_files.abcd-hcp-pipeline_infant
+
+module load singularity
+
+file=run${SLURM_ARRAY_TASK_ID}
+
+bash ${file}

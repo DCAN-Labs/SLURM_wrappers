7afb3e0b3aaa32bc2d907a9292c9edb5831e7519
rae McCollum <mccol199@ln1001.mangi.msi.umn.edu>
2023-10-10 12:07:55 -0500

    added remaining misc wrappers

---
+++ slurm_motion_tsv_old_abcd-hcp-pipeline_outputs/make_run_files.sh
@@ -0,0 +1,40 @@
+ #!/bin/bash 
+set +x 
+
+# determine data directory, run folders, and run templates
+run_folder=`pwd`
+
+dicom2bids_folder="${run_folder}/run_files.dicom2bids"
+dicom2bids_template="template.reformat"
+dicom2bids_subjects="${run_folder}/sub_files.dicom2bids"
+
+bucket=s3://ABCC_year2
+session=2YearFollowUpYArm1
+
+group=`groups|cut -d" " -f1`
+
+# if processing run folders (sMRI, fMRI,) exist delete them and recreate
+if [ -d "${dicom2bids_folder}" ]; then
+	rm -rf "${dicom2bids_folder}"
+	mkdir -p "${dicom2bids_folder}/logs"
+else
+	mkdir -p "${dicom2bids_folder}/logs"
+fi
+
+if [ ! -d ${dicom2bids_subjects} ]; then
+	mkdir -p ${dicom2bids_subjects}
+fi
+
+# counter to create run numbers
+k=0
+
+cat /home/rando149/shared/projects/ABCC_year2_motiontsv/processed_subjects.txt | while read line; do
+	echo ${line} > ${dicom2bids_subjects}/sub-${line}.txt
+	sed -e "s|SUBJECTID|${line}|g" -e "s|SUBJECTDIR|${dicom2bids_subjects}|g" -e "s|BUCKETPATH|${bucket}|g" -e "s|SESSIONID|${session}|g" ${run_folder}/template.reformat > ${dicom2bids_folder}/run${k}
+
+	k=$((k+1))
+done
+
+chmod +x -R ${dicom2bids_folder} 
+
+
---
+++ slurm_move_subjects/resources_move_subjects.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH -J move_subjects
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=12gb
+#SBATCH --tmp=200gb
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lundq163@umn.edu
+#SBATCH -p msismall
+#SBATCH -o output_logs/abcd-hcp-pipeline_full_%A_%a.out
+#SBATCH -e output_logs/abcd-hcp-pipeline_full_%A_%a.err
+#SBATCH -A rando149
+
+cd run_files.move_subjects
+
+module load singularity
+
+file=run${SLURM_ARRAY_TASK_ID}
+
+bash ${file}
+
+# mesabi partitions for -p are amdsmall,amd512,amd2tb,ram256g,ram1t

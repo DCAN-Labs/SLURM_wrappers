e826a877fb543735a15d600398a0f869482de490
rae McCollum <mccol199@ln1001.mangi.msi.umn.edu>
2023-10-10 12:03:05 -0500

    added prep wrappers, deleted logs/run files

---
+++ slurm_nibabies_scripts_with_s3_routines_with_ses/resources_nibabies_full_run_low_resources.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -J nibabies_low
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=240G
+#SBATCH -t 48:00:00
+#SBATCH -p msismall
+#SBATCH -o output_logs/nibabies_low_%A_%a.out
+#SBATCH -e output_logs/nibabies_low_%A_%a.err
+#SBATCH -A rando149
+
+cd run_files.nibabies_full_low_resources
+
+module load singularity
+
+file=run${SLURM_ARRAY_TASK_ID}
+
+bash ${file}

ae61dace0a20e2b5ce78667d3f3d349cb72b9db0
rae McCollum <mccol199@acn06.agate.msi.umn.edu>
2023-10-25 12:00:29 -0500

    reorganized abcd wrappers

---
+++ infant_wrappers/slurm_nibabies_and_xcpd_scripts_with_s3_routines_with_ses_TM_test/resources_nibabies_full_run_copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH -J nibabies_full
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 120
+#SBATCH --mem=960G
+#SBATCH -t 18:00:00
+#SBATCH -p ag2tb
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tmadison@umn.edu
+#SBATCH -o output_logs/nibabies_full_%A_%a.out
+#SBATCH -e output_logs/nibabies_full_%A_%a.err
+#SBATCH -A znahas
+
+cd run_files.nibabies_full
+
+module load singularity
+
+file=run${SLURM_ARRAY_TASK_ID}
+
+bash ${file}
---
+++ infant_wrappers/slurm_nibabies_and_xcpd_with_s3_routines_with_ses/README.md
@@ -0,0 +1,2 @@
+# slurm_abcd-hcp-pipeline_scripts
+This slurm wrapper pulls down a single subjects from the s3 at a time. It runs nibabies then xcp before syncing the outputs back to the s3. 
